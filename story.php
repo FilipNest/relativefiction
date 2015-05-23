1d19b0d03332600a52db962f1bd554a9e801c973
Filip Hnízdo <filip@bluejumpers.com>
2015-05-23 15:13:21 +0100

    Make multiple Foursquare venue calls if needed

--- story.php
+++ story.php
@@ -484,128 +484,167 @@ foreach($variables as $variable){
     
   }
   
-} 
+}
 
 //Make bulk request for all Foursquare categories in text
 
-$request = new HTTP_Request2('https://api.foursquare.com/v2/venues/search',
-                             HTTP_Request2::METHOD_GET, array('use_brackets' => true));
-
-$request->setConfig(array(
-    'ssl_verify_peer'   => FALSE,
-    'ssl_verify_host'   => FALSE
-));
-
-$url = $request->getUrl();
-$url->setQueryVariables(array(
-
-    "client_id" => $foursquare['id'],
-    "client_secret" => $foursquare['secret'],
-  "limit" => 50,
-    "v" => "20150516",
-    "ll" => $location["latitude"].",".$location["longitude"],
-  "categoryId" => implode(",",$categoryids)
-
-));
-
-$places = $request->send()->getBody();
-
-$places = json_decode($places)->response->venues;
-
-//Sort into array
-
-$fetchedvenues = array();
-
-foreach ($places as $place){
-    
-  foreach ($place->categories as $category){
-      
-  if(!isset($fetchedvenues[$category->id])){
-  
-    $fetchedvenues[$category->id] = array();
-    
-  }
-  
-     $fetchedvenues[$category->id][] = $place;
-    
-  }
-  
-};
-
+$extraplaces = array();
 
 function cmp($a, $b)
-{
-    if ($a->location->distance == $b->location->distance) {
-        return 0;
-    }
-    return ($a->location->distance < $b->location->distance) ? -1 : 1;
-}
+  {
+      if ($a->location->distance == $b->location->distance) {
+          return 0;
+      }
+      return ($a->location->distance < $b->location->distance) ? -1 : 1;
+  }
 
-//Loop over variables to swap in places
+function parselocations(){
 
-foreach($placevariables as $place){
+  global $extraplaces;
+  global $foursquare;
+  global $location;
+  global $output;
+  global $categoryids;
+  global $placevariables;
   
-  $tag = $place["tag"];
-  $category = $place["category"];
-  $id = $place["id"] - 1;
-  $extra = $place["extra"];
+  $request = new HTTP_Request2('https://api.foursquare.com/v2/venues/search',
+                               HTTP_Request2::METHOD_GET, array('use_brackets' => true));
+
+  $request->setConfig(array(
+      'ssl_verify_peer'   => FALSE,
+      'ssl_verify_host'   => FALSE
+  ));
+
+  $url = $request->getUrl();
+  $url->setQueryVariables(array(
+
+      "client_id" => $foursquare['id'],
+      "client_secret" => $foursquare['secret'],
+    "limit" => 50,
+      "v" => "20150516",
+      "ll" => $location["latitude"].",".$location["longitude"],
+    "categoryId" => implode(",",$categoryids)
+
+  ));
+
+  $places = $request->send()->getBody();
+
+  $places = json_decode($places)->response->venues;
+
+  //Sort into array
+
+  $fetchedvenues = array();
+
+  foreach ($places as $place){
+
+    foreach ($place->categories as $category){
+
+    if(!isset($fetchedvenues[$category->id])){
+
+      $fetchedvenues[$category->id] = array();
+
+    }
+
+       $fetchedvenues[$category->id][] = $place;
+
+    }
+
+  };
+
+  //Loop over variables to swap in places
+
+  foreach($placevariables as $place){
+
+    $tag = $place["tag"];
+    $category = $place["category"];
+    $id = $place["id"] - 1;
+    $extra = $place["extra"];
+
+    if(isset($fetchedvenues[$place["category"]]) && isset($fetchedvenues[$place["category"]][$id])){
+
+      usort($fetchedvenues[$place["category"]], "cmp");
+
+      $venue = $fetchedvenues[$place["category"]][$id]->name;
+
+      //Rewrite to extra if needed
+
+      if($extra == "distance"){
+
+        $venue = $fetchedvenues[$place["category"]][$id]->location->distance;
+
+      }
+
+      if($extra == "street"){
+
+        if(isset($fetchedvenues[$place["category"]][$id]->location->address)){
+
+        $venue = $fetchedvenues[$place["category"]][$id]->location->address;
+
+        } else {
+
+          $venue = "the street";
+
+        }
+
+        //Strip out words containing numbers
+         preg_match_all("/(^[\D]+\s|\s[\D]+\s|\s[\D]+$|^[\D]+$)+/",$venue,$result);
+  $venue = implode('',$result[0]);
+
+        //Trim
+
+        $venue = trim($venue);
+
+      }
+
+      if($extra == "city"){
+
+        if(isset($fetchedvenues[$place["category"]][$id]->location->city)){
+
+        $venue = $fetchedvenues[$place["category"]][$id]->location->city;
+
+        } else {
+
+          $venue = "the city";
+
+        }
+
+      }
+
+      $output = str_replace("[".$tag."]", $venue, $output);
+
+    } else {
+      
+      //Slot in extra places.
+     
+      $extraplaces[] = $place;
+      
+    }
+ }
+  
+  if(isset($extraplaces) && count($extraplaces) > 0){
+            
+  $placevariables = $extraplaces;
+  $extraplaces = array();
+  $categoryids = array();
+  
+  foreach($placevariables as $place){
    
-  if(isset($fetchedvenues[$place["category"]]) && isset($fetchedvenues[$place["category"]][$id])){
-    
-    usort($fetchedvenues[$place["category"]], "cmp");
-    
-    $venue = $fetchedvenues[$place["category"]][$id]->name;
-    
-    //Rewrite to extra if needed
-    
-    if($extra == "distance"){
-      
-      $venue = $fetchedvenues[$place["category"]][$id]->location->distance;
-      
+    if (in_array($place['category'], $categoryids) == false) {
+      $categoryids[] = $place['category'];
+
     }
-    
-    if($extra == "street"){
-      
-      if(isset($fetchedvenues[$place["category"]][$id]->location->address)){
-      
-      $venue = $fetchedvenues[$place["category"]][$id]->location->address;
-        
-      } else {
-       
-        $venue = "the street";
-        
-      }
-      
-      //Strip out words containing numbers
-       preg_match_all("/(^[\D]+\s|\s[\D]+\s|\s[\D]+$|^[\D]+$)+/",$venue,$result);
-$venue = implode('',$result[0]);
-       
-      //Trim
-      
-      $venue = trim($venue);
-      
-    }
-    
-    if($extra == "city"){
-      
-      if(isset($fetchedvenues[$place["category"]][$id]->location->city)){
-      
-      $venue = $fetchedvenues[$place["category"]][$id]->location->city;
-      
-      } else {
-       
-        $venue = "the city";
-        
-      }
-      
-    }
-    
-    $output = str_replace("[".$tag."]", $venue, $output);
     
   }
   
+  parselocations();
+  
 }
 
+}
+
+parselocations();
+
+  
 //Onto conditionals!
 
 preg_match_all("/\{if([^\]]*)\}/", $output, $conditionals);
