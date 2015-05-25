852bd6ea2ad02b8fa0155229f05cb980c3db55d9
Filip Hnízdo <filip@bluejumpers.com>
2015-05-25 14:08:15 +0100

    Offset time

--- dates.php
+++ dates.php
@@ -4,28 +4,43 @@ register('dayofweek', function($variable){
   
   global $time;
   
-  //Check if offset is set
+  $date = new DateTime("@$time"); 
   
-  $offset = 0;
+  //Check if offset is set
   
   if(count(explode("|",$variable)) > 1){
    
     $offset = explode("|",$variable)[1];
     
+    intval($offset);
     
-    $offset = intval($offset*86400);
+    date_add($date, date_interval_create_from_date_string($offset.' days'));
     
   }
   
-  return date("l",$time + $offset); 
+  return date_format($date, 'l'); 
   
 });
  
-register('minutes', function(){
+register('minutes', function($variable){
   
-  global $time;
+global $time;
   
-  return date("i",$time);
+  $date = new DateTime("@$time"); 
+  
+  //Check if offset is set
+  
+  if(count(explode("|",$variable)) > 1){
+   
+    $offset = explode("|",$variable)[1];
+    
+    intval($offset);
+    
+    date_add($date, date_interval_create_from_date_string($offset.' minutes'));
+    
+  }
+  
+  return date_format($date, 'i'); 
   
 });
 
@@ -33,7 +48,21 @@ register('monthofyear', function($variable){
   
   global $time;
   
-  return date("F",$time);
+  $date = new DateTime("@$time"); 
+  
+  //Check if offset is set
+  
+  if(count(explode("|",$variable)) > 1){
+   
+    $offset = explode("|",$variable)[1];
+    
+    intval($offset);
+    
+    date_add($date, date_interval_create_from_date_string($offset.' months'));
+    
+  }
+  
+  return date_format($date, 'F'); 
   
 });
 
@@ -41,7 +70,21 @@ register('year', function($variable){
   
   global $time;
   
-  return date("Y",$time);
+  $date = new DateTime("@$time"); 
+  
+  //Check if offset is set
+  
+  if(count(explode("|",$variable)) > 1){
+   
+    $offset = explode("|",$variable)[1];
+    
+    intval($offset);
+    
+    date_add($date, date_interval_create_from_date_string($offset.' years'));
+    
+  }
+  
+  return date_format($date, 'Y'); 
   
 });
 
@@ -49,7 +92,21 @@ register('hours12', function($variable){
   
   global $time;
   
-  return ltrim(date("h", $time), '0');
+  $date = new DateTime("@$time"); 
+  
+  //Check if offset is set
+  
+  if(count(explode("|",$variable)) > 1){
+   
+    $offset = explode("|",$variable)[1];
+    
+    intval($offset);
+    
+    date_add($date, date_interval_create_from_date_string($offset.' hours'));
+    
+  }
+  
+  return ltrim(date_format($date, 'h'), '0');
   
 });
 
@@ -57,15 +114,43 @@ register('hours24', function($variable){
   
   global $time;
   
-  return date("H", $time);
+  $date = new DateTime("@$time"); 
+  
+  //Check if offset is set
+  
+  if(count(explode("|",$variable)) > 1){
+   
+    $offset = explode("|",$variable)[1];
+    
+    intval($offset);
+    
+    date_add($date, date_interval_create_from_date_string($offset.' hours'));
+    
+  }
+  
+    return date_format($date, 'H'); 
   
 });
 
 register('hoursampm', function($variable){
   
-  global $time;
+    global $time;
   
-  return date("a", $time);
+  $date = new DateTime("@$time"); 
+  
+  //Check if offset is set
+  
+  if(count(explode("|",$variable)) > 1){
+   
+    $offset = explode("|",$variable)[1];
+    
+    intval($offset);
+    
+    date_add($date, date_interval_create_from_date_string($offset.' hours'));
+    
+  }
+  
+    return date_format($date, 'a');
   
 });
 
