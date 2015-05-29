f951a1d0ccf0d75375e7b4d9077d96ba248a63fb
Filip Hnízdo <filip@bluejumpers.com>
2015-05-29 18:04:13 +0100

    Book typography changes

--- rf1/header.php
+++ rf1/header.php
@@ -1,7 +1,13 @@
 <!doctype HTML>
 <html>
 <head>
-  <header><section><span><strong>Here.</strong> A collection of relative fiction</span><span id="localstories">Made with <a href="http://filipnest.com/localstories">Local stories</a></span></section></header>
+    <script src="//use.typekit.net/vct1gvu.js"></script>
+  <script>
+    try {
+      Typekit.load();
+    } catch (e) {}
+  </script>
+  <header><section><span><strong>Here.</strong> A collection of relative fiction.</span><span id="localstories">Made with <a href="http://filipnest.com/localstories">Local stories</a></span></section></header>
   <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
   <link rel="stylesheet" href="stories.css">
 </head>
