8163bf53b67b7644f2f52ee756a7026b29dbe0d4
Filip Hnízdo <filip@bluejumpers.com>
2015-05-30 02:32:14 +0100

    Book front page

---
+++ rf1/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+
+<head>
+  <script src="//use.typekit.net/vct1gvu.js"></script>
+  <script>
+    try {
+      Typekit.load();
+    } catch (e) {}
+  </script>
+  <link rel="stylesheet" href="stories.css" />
+</head>
+
+<body class="front">
+
+  <header>
+    <section><span><strong></strong>&nbsp;</span><span id="localstories">Made with <a href="http://filipnest.com/localstories">Local stories</a></span>
+    </section>
+  </header>
+  <section id="home">
+    <section id="cover">
+      <a href="story.php"><h1>Here.</h1>
+    <h2>A collection of relative fiction</h2>
+    <h3>Featuring stories by:</h3>
+    <ul>
+      <li>Filip Hn&#237;zdo</li>
+      <li>Chloe Aust</li>
+      <li>Sam Davey</li>
+      <li>Tanya Perdikou</li>
+      <li>Heather Shaw</li>
+      <li>Peter Lloyd Morgan</li>
+      <li>Amy Gibb</li>
+      <li>Louise Cooke</li>
+      <li>Fiona Shipwright</li>
+      <li>Anna Hunter</li>
+    </ul>
+      </a>
+    </section>
+    <section id="intro">
+<p><b>Here</b> is a series of relative fiction written by ten authors commissioned to write a short story to launch the local stories relative fiction engine.</p>
+      <p>Select an author's name from the front of the book or simply flick through to explore the stories.</p>
+      <p>For the stories to work you will need to have a web browser with geolocation functionality.</p>
+    </section>
+  </section>
+
+</body>
+
+</html>
