4ce0ecd48d5ee3c99dd6126116f9be98ac333d0f
Filip Hnízdo <filip@bluejumpers.com>
2015-05-31 21:54:58 +0100

    Default secrets file

---
+++ secrets-rename.php
@@ -0,0 +1,18 @@
+<?php
+
+//Rename this file to secrets.php and fill in the API keys once you have registered for them.
+
+//Foursquare API key
+
+$foursquare = array(
+ 
+  "id" => "",
+  "secret" => ""
+  
+);
+
+//Open weather map API key
+
+$openweathermap = "";
+
+?>
