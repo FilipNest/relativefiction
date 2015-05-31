4604b2d8ff9d68a7c292839e5f5ad10caf8ee6bb
Filip Hnízdo <filip@bluejumpers.com>
2015-05-31 22:17:52 +0100

    Google analytics

--- here/index.php
+++ here/index.php
@@ -39,6 +39,17 @@
     <footer>Each story is the copyright of its individual author. The Local stories software is released for free under the <a href="http://choosealicense.com/licenses/apache-2.0/">Apache License 2.0</a>. </footer>
   </section>
 
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-63609457-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+  
 </body>
 
 </html>
