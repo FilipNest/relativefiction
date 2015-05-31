f93962fbba9443dd09311d1be3477d5eae7d99b8
Filip Hnízdo <filip@bluejumpers.com>
2015-05-31 22:49:15 +0100

    Text changes on intro

--- here/index.php
+++ here/index.php
@@ -28,12 +28,12 @@
       <a href="dangers.php"><li>Chloe Aust</li></a>
       <a href="jasmine.php"><li>Sam Davey</li></a>
       <a href="ideasabovestations.php"><li>Fiona Shipwright</li></a>
-      <li>And you? <a href="mailto:filip@bluejumpers.com"><b>Get in touch.</b></a></li>
+      <li>And you? <a href="mailto:filip@bluejumpers.com"><b>Submit a story.</b></a></li>
     </ul>
     </section>
     <section id="intro">
 <p><b>Here</b> is a collection of short fiction written for the launch of <a href="http://relativefiction.com">Local stories</a>, a piece of (currently experimental) software that enables people to localise words to a reader's place, time and weather.</p>
-      <p>Please select an author's name from the front of the book to explore the stories or select the title to explore from the beginning.</p>
+      <p>Please select an author's name to explore the stories or select the title to explore from the beginning.</p>
       <p>You will need to have a web browser that allows you to share your location (most do) and give permission to the website to use this information. These geo-coordinates are not stored to any database and are anonymous.</p>
     </section>
     <footer>Each story is the copyright of its individual author. The Local stories software is released for free under the <a href="http://choosealicense.com/licenses/apache-2.0/">Apache License 2.0</a>. </footer>
