53034f6c72ef0d955d0b2219ab17bcedae9f2523
Filip Hnízdo <filip@bluejumpers.com>
2015-06-25 20:33:20 +0100

    Tanya's name in index

--- here/index.php
+++ here/index.php
@@ -29,6 +29,7 @@
       <a href="jasmine.php"><li>Sam Davey</li></a>
       <a href="ideasabovestations.php"><li>Fiona Shipwright</li></a>
       <a href="againstme.php"><li>Amy Gibb</li></a>
+      <a href="she.php"><li>Tanya Perdikou</li></a>
       <li>And you? This is a new thing. Have a read of how the system works, write a story and <a href="mailto:filip@bluejumpers.com"><b>send it in.</b></a></li>
     </ul>
     </section>
