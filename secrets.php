353f1288faf55ad321c823cfd4bfe0a8d55eba9b
root <root@hnzdoaa1.miniserver.com>
2015-09-12 12:38:07 +0000

    server sync

---
+++ secrets.php
@@ -0,0 +1,12 @@
+<?php
+
+$foursquare = array(
+ 
+  "id" => "GLVSRGETGQAOFL01NNFNPPIG3RUFRIBCKWPXJSMSYUVD15V2",
+  "secret" => "LXUN1X2B4DRF2EKMURYEZRPYBLML2MND5ZKOKWILD5KSZ2EE"
+  
+);
+
+$openweathermap = "4e36efb3cd43a9037a66de5e5d054493";
+
+?>
