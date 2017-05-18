40937fc0d4b2520dd13c13dfaa53162eb5e97aba
Filip Hnízdo <filip@bluejumpers.com>
2017-05-18 14:35:49 +0100

    Tag descriptions

--- misc.js
+++ misc.js
@@ -2,10 +2,10 @@ rf.tag("latitude", function (tagParams, output) {
 
   return output.latitude;
 
-});
+}, "Shows the current latitude.");
 
 rf.tag("longitude", function (tagParams, output) {
 
   return output.longitude;
 
-});
+}, "Shows the current longitude.");
