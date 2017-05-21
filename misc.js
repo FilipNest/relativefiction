b9b6192b13f3d914dde6c53dc4eaba67818f7653
Filip Hnízdo <filip@bluejumpers.com>
2017-05-21 22:49:19 +0100

    URL fetch

--- misc.js
+++ misc.js
@@ -21,3 +21,43 @@ rf.tag("country", function (tagParams, output) {
 }, {
   category: "general"
 });
+
+var request = require("request");
+
+rf.tag("url", function (tagParams, output) {
+  
+  return new Promise(function (pass, fail) {
+
+    if (typeof tagParams[0] !== "string") {
+
+      fail("Must pass in a url");
+
+    }
+
+    try {
+
+      request(tagParams[0], function (error, response, body) {
+        
+        if (error) {
+          
+          fail(error.message);
+
+        } else {
+
+          pass(body);
+
+        }
+
+      })
+
+    } catch (e) {
+      
+      fail(e.message);
+
+    }
+
+  })
+
+}, {
+  category: "general"
+})
